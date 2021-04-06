6afae3691926649da817fc71e93b711840eee46a
Pavel <lenkopv@google.com>
2021-04-06 22:31:57 +0300

    task1_1

---
+++ src/task1_1.java
@@ -0,0 +1,19 @@
+public class task1_1 {
+
+    public static void main(String[] args) {
+
+        //1. Объявите две переменные: admin и name.
+        String admin;
+        String name;
+
+        //2. Запишите строку "Ivan" в переменную name.
+        name = "Ivan";
+
+        //3. Скопируйте значение из переменной name в admin.
+        admin = name;
+
+        //4. Выведите в терминл значение admin
+        System.out.println(admin);
+
+    }
+}
