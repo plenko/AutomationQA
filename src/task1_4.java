b199b3d532478c92188e5e9be54a4991f77726fd
Pavel <lenkopv@google.com>
2021-04-06 23:05:43 +0300

    task1_4

---
+++ src/task1_4.java
@@ -0,0 +1,9 @@
+public class task1_4 {
+
+    public static void main(String[] args) {
+        //Прибавьте к значению переменной z  такое число, чтобы получилось -2147483648
+        int z = 2147483647;
+        z = z + (-2 * z -1);
+        System.out.println(z);
+    }
+}
