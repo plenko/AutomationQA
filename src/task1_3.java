c2c9b8c0a16f98d1651aebe7d1204287551d9708
Pavel <lenkopv@google.com>
2021-04-06 23:01:27 +0300

    task1_3

---
+++ src/task1_3.java
@@ -0,0 +1,13 @@
+public class task1_3 {
+    public static void main(String[] args) {
+        //Исправьте код программы, что результат деления 5 на 7 , давал правильный результат, а не 0
+        double x = 5;
+        double y = 7;
+        double z = 0;
+
+        z = x / y;
+
+        System.out.println (z);
+
+    }
+}
