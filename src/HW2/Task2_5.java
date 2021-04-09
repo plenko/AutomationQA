d9c39f1d07df27a97095c541e518684051dc18e6
Pavel <lenkopv@google.com>
2021-04-10 00:24:03 +0300

    v1

---
+++ src/HW2/Task2_5.java
@@ -0,0 +1,28 @@
+package HW2;
+
+import java.util.Scanner;
+
+public class Task2_5 {
+
+    public static void main(String[] args) {
+
+        /*
+Даны 2 числа (a и b)
+Если число а четное, то нужно вывести в консоль произведение чисел a и b
+Если число a нечетное, то нужно вывести в консоль сумму чисел a и b
+
+*/
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int a = in.nextInt();
+        System.out.print("Введите второе число: ");
+        int b = in.nextInt();
+
+        if (a % 2 == 0){
+            System.out.println("Результат: " + a*b);
+        } else {
+            System.out.println("Результат: " + (a+b));
+        }
+
+    }
+}
