25db546652a94a6a1e5f8143b6e90c703a2f60a5
Pavel <lenkopv@google.com>
2021-04-09 23:27:47 +0300

    v1

---
+++ src/HW2/Task2_1b.java
@@ -0,0 +1,27 @@
+package HW2;
+
+public class Task2_1b {
+    public static void main(String[] args) throws Exception{
+        /*Имеется прямоугольное отверстие размерами a и b.
+Определите можно ли его полностью закрыть круглой картонкой радиусом r.
+Вывести результат на экран.
+
+Данные a, b и r ввести с консоли.
+*/
+        System.out.print("Введите радиус r: ");
+        int r = System.in.read();
+
+        System.out.print("Введите сторону a: ");
+        int a = System.in.read();
+
+        System.out.print("Введите сторону b: ");
+        int b = System.in.read();
+
+        /*if (2 *  r >= Math.sqrt(a*a + b*b))
+            System.out.println("Можно");
+        else
+            System.out.println("Нельзя");*/
+
+    }
+
+}
---
+++ src/HW2/Task2_2.java
@@ -0,0 +1,44 @@
+package HW2;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task2_2 {
+
+    public static void main(String[] args) throws IOException {
+
+        /*Имеется целое число n < 10.
+Вывести какому дню недели оно соответствует, если при n = 1 день недели = Понедельник.
+*/
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите номер дня от 1 до 9: ");
+        int d = in.nextInt();
+        String dayOfWeek;
+
+        switch (d) {
+            case 1: dayOfWeek = "Понедельник";
+                break;
+            case 2: dayOfWeek = "Вторник";
+                break;
+            case 3: dayOfWeek = "Среда";
+                break;
+            case 4: dayOfWeek = "Четверг";
+                break;
+            case 5: dayOfWeek = "Пятница";
+                break;
+            case 6: dayOfWeek = "Суббота";
+                break;
+            case 7: dayOfWeek = "Воскресение";
+                break;
+            case 8: dayOfWeek = "Понедельник";
+                break;
+            case 9: dayOfWeek = "Вторник";
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + d);
+        }
+        System.out.println(dayOfWeek);
+
+    }
+
+}
