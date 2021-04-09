910bf918148968b1b3697b24f94be27d87ad1ac8
Pavel <lenkopv@google.com>
2021-04-10 00:28:30 +0300

    v1

--- src/HW2/Task2_5.java
+++ src/HW2/Task2_5.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 public class Task2_5 {
 
     public static void main(String[] args) {
-
         /*
 Даны 2 числа (a и b)
 Если число а четное, то нужно вывести в консоль произведение чисел a и b
 Если число a нечетное, то нужно вывести в консоль сумму чисел a и b
-
 */
         Scanner in = new Scanner(System.in);
         System.out.print("Введите первое число: ");
